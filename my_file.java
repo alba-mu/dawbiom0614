7719429daf1ab96b823f4206467d4fa585e36053
Alba Muñoz <amu2760@proven.local>
2025-09-17 17:44:08 +0200

    Adding modification to my_file.java

--- my_file.java
+++ my_file.java
@@ -2,5 +2,6 @@ public class my_file{
 	public static void main(String args[]){
 		System.out.println("First project git");
 		System.out.println("New line");
+		System.out.println("Adding future plans");
 	}
 }
