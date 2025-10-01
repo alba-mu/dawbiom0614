68a18bc03ff7dae0378a05f8cb960b66b3109eb2
Alba Muñoz Ordoño <amu2760@proven.local>
2025-10-01 17:45:00 +0200

    Reesctructuració fitxers

---
+++ project_ini/my_file.java
@@ -0,0 +1,9 @@
+public class my_file{
+	public static void main(String args[]){
+		System.out.println("First project git");
+		System.out.println("New line");
+		System.out.println("Adding future plans");
+		System.out.println("Show on diff");
+		System.out.println("No conflict");
+	}
+}
---
+++ project_ini/my_second_file.java
@@ -0,0 +1,6 @@
+public class My_second_file{
+	String name;
+	int edat;
+	int subjects;
+	double price;
+}
